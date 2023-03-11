87ad6eb42e7a53aac6a61e7b4ea192d3fe56bdf3
Franco_H <francoh.jobs@gmail.com>
2023-03-10 21:44:05 -0500

    improvements to for loops list, shorthand version

--- fundamentals/fundamentals/nested_dictionaries/nested_dict_and_lists.py
+++ fundamentals/fundamentals/nested_dictionaries/nested_dict_and_lists.py
@@ -26,27 +26,27 @@
 
 
 #ITERATE THROUGH A LIST OF DICTIONARIES
-# students = [
-#         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#         {'first_name' : 'John', 'last_name' : 'Rosales'},
-#         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#         {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
-# # should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# # bonus to get them to appear exactly as below!)
-# # first_name - Michael, last_name - Jordan
-# # first_name - John, last_name - Rosales
-# # first_name - Mark, last_name - Guillen
-# # first_name - KB, last_name - Tonel
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
-# def iterateDictionary(students_list):
-#     for i in range(len(students_list)):
-#         first_and_last_name = ""
-#         for key, value in students_list[i].items():
-#             first_and_last_name += f"{key} - {value}, "
-#         print(first_and_last_name[:-2])
+def iterateDictionary(students_list):
+    for dictionary in students_list:
+        first_and_last_name = ""
+        for key, value in dictionary.items():
+            first_and_last_name += f"{key} - {value}, "
+        print(first_and_last_name[:-2])
 
-# iterateDictionary(students)
+iterateDictionary(students)
 
 #GET VALUES IN A LIST
 # students = [
@@ -61,48 +61,54 @@
 # # For example, iterateDictionary2('first_name', students) should output:
 
 # def iterateDictionary2(key,list_of_dicts):
-#     for integer in range(len(list_of_dicts)):
-#         keys = list_of_dicts[integer][key]
+#     for dictionary in list_of_dicts:
+#         keys = dictionary[key]
 #         print(keys)
 
 # iterateDictionary2("last_name", students)
 
 #ITERATE THROUGH A DICTIONARY WITH LIST VALUES
-dojo = {
-    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
+# dojo = {
+#     'locations': ["San Jose", "Seattle", "Dallas", "Chicago", "Tulsa", "DC", "Burbank"],
+#     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
 
-def printInfo(dojo):
-    for key, value in dojo.items():
-        # print key lenght and name
-        length_of_key = len(value)
-        key = key.upper()
-        length_plus_key_name = f"{length_of_key} {key}"
-        print("")
-        print(length_plus_key_name)
-        for i in range(len(value)):
-            value_listed = value[i]
-            print(value_listed)
+# def printInfo(dojo):
+#     for key, value in dojo.items():
+#         # print key lenght and name
+#         length_of_key = len(value)
+#         key = key.upper()
+#         length_plus_key_name = f"{length_of_key} {key}"
+#         print("")
+#         print(length_plus_key_name)
+#         # this is the long way
+#         # for i in range(len(value)):
+#         #     value_listed = value[i]
+#         #     print(value_listed)
+#         #short way, since python automatically re-assigns the value of each index to i
+#         #but only when writen this way:
+#         for i in value:
+#             value_listed = i
+#             print(value_listed)
     
-printInfo(dojo)
 # printInfo(dojo)
-# # output:
-# 7 LOCATIONS
-# San Jose
-# Seattle
-# Dallas
-# Chicago
-# Tulsa
-# DC
-# Burbank
+# # printInfo(dojo)
+# # # output:
+# # 7 LOCATIONS
+# # San Jose
+# # Seattle
+# # Dallas
+# # Chicago
+# # Tulsa
+# # DC
+# # Burbank
     
-# 8 INSTRUCTORS
-# Michael
-# Amy
-# Eduardo
-# Josh
-# Graham
-# Patrick
-# Minh
-# Devon
+# # 8 INSTRUCTORS
+# # Michael
+# # Amy
+# # Eduardo
+# # Josh
+# # Graham
+# # Patrick
+# # Minh
+# # Devon
