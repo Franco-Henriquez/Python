b835a256ef60b517b84492311b4edf9773af3e92
Franco_H <francoh.jobs@gmail.com>
2023-04-03 02:41:23 -0400

    added more structure and bootstrap shortcodes

--- flask_mysql/crud/Users_CR/server.py
+++ flask_mysql/crud/Users_CR/server.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 # import the class from friend.py
 from user import User
 app = Flask(__name__)
-@app.route("/")
-def index():
+
+@app.route("/users")
+def user_list():
     # call the get all classmethod to get all friends
     users = User.get_all()
     # print('User:',users[0].first_name)
-    return render_template("index.html", all_users = users)
-            
+    return render_template("users.html", all_users = users)
+
+@app.route("/users/new")
+def new_user():
+    return render_template("add_user.html")
+
+@app.route("/users/create", methods=['POST'])
+def create_user():
+    User.add_user(request.form)
+    return redirect('/users')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
--- flask_mysql/crud/Users_CR/user.py
+++ flask_mysql/crud/Users_CR/user.py
@@ -2,6 +2,7 @@
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
 class User:
+    DB = "facegram"
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -16,12 +17,20 @@ class User:
         query = "SELECT * FROM users;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         # aka the database name, in this case, facegram
-        results = connectToMySQL('facegram').query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query)
         # Create an empty list to append our instances of friends
         users = []
         # Iterate over the db results and create instances of friends with cls.
         for user in results:
             users.append( cls(user) )
-            print("User debug from friend.py:",users[0].first_name)
+            # print("User debug from user.py:",users[0].first_name)
         return users
+    
+    @classmethod
+    def add_user(cls, data):
+        query = """INSERT INTO users (first_name,last_name,email,password)
+    		VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"""
+        result = connectToMySQL(cls.DB).query_db(query,data)
+        print(result)
+        return result
             
