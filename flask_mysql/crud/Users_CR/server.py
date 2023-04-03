55c276cbb2614d1a8e258cedbb84a862955bb40e
Franco_H <francoh.jobs@gmail.com>
2023-04-03 01:07:26 -0400

    users project

---
+++ flask_mysql/crud/Users_CR/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+# import the class from friend.py
+from user import User
+app = Flask(__name__)
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    users = User.get_all()
+    # print('User:',users[0].first_name)
+    return render_template("index.html", all_users = users)
+            
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ flask_mysql/crud/Users_CR/user.py
@@ -0,0 +1,27 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        # aka the database name, in this case, facegram
+        results = connectToMySQL('facegram').query_db(query)
+        # Create an empty list to append our instances of friends
+        users = []
+        # Iterate over the db results and create instances of friends with cls.
+        for user in results:
+            users.append( cls(user) )
+            print("User debug from friend.py:",users[0].first_name)
+        return users
+            
