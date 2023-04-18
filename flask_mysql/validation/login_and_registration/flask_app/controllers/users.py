70d23eed8e340274d1bec6598ba0267c85fa7277
Franco_H <francoh.jobs@gmail.com>
2023-04-17 21:53:47 -0400

    update

--- flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -28,12 +28,20 @@ def register():
 @app.route('/login',methods=['POST'])
 def login():
     user = User.get_by_email(request.form)
+    # because we need to redirect to our root when either the email or 
+    # password is invalid (doesn't exist in db)
+    # we write the if statement here with the flash messages in our controller
+    # rather than in our model
     if not user:
         flash("Invalid Email","login")
         return redirect('/')
     if not bcrypt.check_password_hash(user.password, request.form['password']):
         flash("Invalid Password","login")
         return redirect('/')
+    
+    #create a session with the newly logged in id.
+    # this will be accessed by the /dashboard
+    # in order to retrieve user information by that id
     session['user_id'] = user.id
     return redirect('/dashboard')
 
@@ -53,5 +61,6 @@ def dashboard():
 
 @app.route('/logout')
 def logout():
+    #simple clear of all cookie sessions and redirect to root
     session.clear()
     return redirect('/')
